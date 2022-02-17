da0be223bd86d1965f75c4d41f967b50fcae0e75
Guillaume <guillaume.jacquart@abtasty.com>
2022-02-17 16:09:26 +0100

    Add decision request proto. Fix protoc version

--- bin/gen-go.sh
+++ bin/gen-go.sh
@@ -7,7 +7,7 @@ update_proto() {
     proto_dir_clean=${1%/}
     echo building $proto_dir_clean go files
     docker run --rm -v $(pwd):$(pwd) -w $(pwd) \
-            namely/protoc \
+            namely/protoc:1.42_2 \
             --go_out=$GOPATH/src/ \
             -I$GOPATH/src/ \
             -I. $proto_dir_clean/$proto_dir_clean.proto
